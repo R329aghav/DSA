b159cffe809153a46736cc117483c82f5e2fcbc4
Raghvendra Singh <79011912+R329aghav@users.noreply.github.com>
2022-01-13 16:40:15 +0530

    Create Pattern3.cpp

---
+++ Pattern3.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+/*
+    *
+    ***
+    *****
+    *******
+    *********
+    ***********
+*/
+int main()
+{
+int n, s, i, j;
+
+cin >> n;
+for(i = 1; i <= n; i++)
+{
+
+for(s = i; s < n; s++)
+{
+cout << " ";
+}
+
+for(j = 1; j <= (2 * i - 1); j++)
+{
+cout << "*";
+}
+
+cout << "\n";
+}
+}
