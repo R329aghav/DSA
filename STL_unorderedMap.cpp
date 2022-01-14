7f7773e78b992df33febcba322c1e96f04368064
Raghvendra Singh <79011912+R329aghav@users.noreply.github.com>
2022-01-14 15:31:43 +0530

    Create STL_unorderedMap.cpp

---
+++ STL_unorderedMap.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+int main(){
+	unordered_map<int ,int>m;
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++){
+		int key,value;
+		cin>>key>>value;
+		m.insert(make_pair(key,value));
+	}
+	cout<<"---------------";
+	if(m.find(2)!=m.end()){
+		cout<<" present"<<endl;
+	}
+	else{
+		cout<<" not present";
+	}
+	for(auto it=m.begin();it!=m.end();it++){
+		cout<<(*it).first<<" "<<(*it).second;
+		cout<<endl;
+	}
+	return 0;
+}
