2309e08a5edc474e9210edfedfcac3eba9bc085f
Raghvendra Singh <79011912+R329aghav@users.noreply.github.com>
2022-01-14 15:30:47 +0530

    Create STL_multiMap.cpp

---
+++ STL_multiMap.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+using namespace std;
+int main(){
+	multimap<int ,int>m;
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++){
+		int key,value;
+		cin>>key>>value;
+		m.insert(make_pair(key,value));
+	}
+	cout<<"---------------";
+	if(m.find(2)!=m.end()){
+		cout<<" present"<<endl;
+	}
+	else{
+		cout<<" not present";
+	}
+	for(auto it=m.begin();it!=m.end();it++){
+		cout<<(*it).first<<" "<<(*it).second;
+		cout<<endl;
+	}
+	return 0;
+}
